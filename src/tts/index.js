e69b7cf2574e3ef1de398f4cfc28e9f761377dff
tsja2001 <52786405+tsja2001@users.noreply.github.com>
2023-03-09 18:41:54 +0800

    fix: 修改代码格式

--- src/tts/index.js
+++ src/tts/index.js
@@ -1,89 +1,57 @@
-// 读取文件
-"use strict"
+"use strict";
 
-require('log-timestamp')(`${process.pid}`)
-const fs = require("fs")
-const Nls = require("alibabacloud-nls")
-const sleep = (waitTimeInMs) => new Promise(resolve => setTimeout(resolve, waitTimeInMs))
-const util = require("util")
-const readline = require("readline")
-const args = process.argv.slice(2)
-//const Memwatch = require("node-memwatch-new")
+const fs = require("fs");
+const Nls = require("alibabacloud-nls");
 
 const URL = "wss://nls-gateway-cn-shanghai.aliyuncs.com/ws/v1";
 const APPKEY = "4lP9qwrK34u5kHHQ"; //获取Appkey请前往控制台：https://nls-portal.console.aliyun.com/applist
 const TOKEN = "dcba785f862a4b7b89478c2cba335742"; //获取Token具体操作，请参见：https://help.aliyun.com/document_detail/450514.html
 
-let b1 = []
-let loadIndex = 0
-//let hd = new Memwatch.HeapDiff()
-let needDump = true
+async function textToSpeech(text, outputFilePath) {
+  console.log(`speak: ${text}`);
 
-async function runOnce(line) {
-  console.log(`speak: ${line}`)
-  loadIndex++
-
-  let dumpFile = fs.createWriteStream(`${process.pid}.wav`, {flags:"w"})
+  let dumpFile = fs.createWriteStream(outputFilePath, { flags: "w" });
   let tts = new Nls.SpeechSynthesizer({
     url: URL,
-    appkey:APPKEY,
-    token:TOKEN
-  })
+    appkey: APPKEY,
+    token: TOKEN,
+    voice: "xiaogang",
+    speech_rate: -500,
+  });
 
-  tts.on("meta", (msg)=>{
-    console.log("Client recv metainfo:", msg)
-  })
+  tts.on("meta", (msg) => {
+    console.log("Client recv metainfo:", msg);
+  });
 
   // 拿到数据
-  tts.on("data", (msg)=>{
-    console.log(`recv size: ${msg.length}`)
-    console.log(dumpFile.write(msg, "binary"))
-  })
+  tts.on("data", (msg) => {
+    // console.log(`recv size: ${msg.length}`)
+    dumpFile.write(msg, "binary");
+  });
 
-  tts.on("completed", (msg)=>{
-    console.log("Client recv completed:", msg)
-  })
+  tts.on("completed", (msg) => {
+    console.log("Client recv completed:", msg);
+    dumpFile.end();
+  });
 
   tts.on("closed", () => {
-    console.log("Client recv closed")
-  })
+    console.log("Client recv closed");
+  });
 
-  tts.on("failed", (msg)=>{
-    console.log("Client recv failed:", msg)
-  })
+  tts.on("failed", (msg) => {
+    console.log("Client recv failed:", msg);
+  });
 
-  let param = tts.defaultStartParams()
-  param.text = line
-  param.voice = "aixia"
+  let param = tts.defaultStartParams();
+  param.text = text;
+  param.voice = "aixia";
   try {
-    await tts.start(param, true, 6000)
-  } catch(error) {
-    console.log("error on start:", error)
-    return
-  } finally {
-    //dumpFile.end()
+    await tts.start(param, true, 6000);
+  } catch (error) {
+    console.log("error on start:", error);
+    return;
   }
-  console.log("synthesis done")
-  await sleep(2000)
+  console.log("synthesis done");
 }
 
-async function test() {
-  console.log("load test case:", args[0])
-  const fileStream = fs.createReadStream(args[0])
-  const rl = readline.createInterface({
-    input: fileStream,
-    crlfDelay: Infinity
-  })
-
-  for await (const line of rl) {
-    b1.push(line)
-  }
-
-  while (true) {
-    for (let text of b1) {
-      await runOnce(text)
-    }
-  }
-}
-
-test()
+textToSpeech("你好, 有什么可以帮助你的吗", `demo_${process.pid}.wav`);
